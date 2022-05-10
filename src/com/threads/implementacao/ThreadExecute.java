959028c294d77327f637c55d1f57ed8a02ad3fed
Gabriel Barbosa Brandão <GabrielBrandao@ufgd.edu.br>
2022-05-10 10:41:26 -0400

    refactor(run): Implementa Zona crítica usando Lock
    melhoria na função run(); aumento do tamanho da matriz; observação da concorrencia sobre a variável em comum; aplica o uso de Lock para controlar o acesso a variável comum
    
    zera variável soma antes de iniciar a busca por numeros primos

--- src/com/threads/implementacao/ThreadExecute.java
+++ src/com/threads/implementacao/ThreadExecute.java
@@ -10,21 +10,24 @@ public class ThreadExecute {
     public static int numeroDeThreads = 0;
     public static ArrayList<ArrayList<Integer>> matrizDeNumeros;
 
-    private static final int rangeNumerosAleatorios = 11; //0 - 10
+    private static final int rangeNumerosAleatorios = 11; // 0 - 10
     private static final Scanner in = new Scanner(System.in);
     private static int somaNumeroDePrimos = 0;
+
+    private static long start, now;
 
     public static void menu() {
         int opcao = 0;
 
-        while (opcao != 6) {
+        while (opcao != 7) {
 
             System.out.println("1 - Definir tamanho da matriz");
             System.out.println("2 - Preencher a matriz com numeros aleatorios");
             System.out.println("3 - Definir número de Threads");
-            System.out.println("4 - Executar");
-            System.out.println("5 - Vizualizar quantidade de números primos e tempo de execução");
-            System.out.println("6 - Encerrar");
+            System.out.println("4 - Executar com Threads");
+            System.out.println("5 - Executar sem Threads");
+            System.out.println("6 - Vizualizar quantidade de números primos e tempo de execução");
+            System.out.println("7 - Encerrar");
 
             opcao = in.nextInt();
 
@@ -36,7 +39,7 @@ public class ThreadExecute {
 
                 case 2:
                     preencherMatriz();
-                    imprimirMatriz();
+                    // imprimirMatriz();
                     break;
 
                 case 3:
@@ -44,14 +47,18 @@ public class ThreadExecute {
                     break;
 
                 case 4:
-                    criarThreads();
+                    executar(true);
                     break;
 
                 case 5:
-                    numerosPrimosETempo();
+                    executar(false);
                     break;
 
                 case 6:
+                    numerosPrimosETempo();
+                    break;
+
+                case 7:
                     break;
 
                 default:
@@ -63,8 +70,16 @@ public class ThreadExecute {
 
     }
 
-    public static void numerosPrimosETempo(){
+    public static void executar(boolean isThread) {
+        if (isThread)
+            criarThreads();
+        else
+            modoSerial();
+    }
+
+    public static void numerosPrimosETempo() {
         System.out.println("A matriz possui = " + ThreadRunnable.getQuantidadeDePrimos());
+        System.out.println("O tempo levado para percorrer toda matriz foi de = " + (now - start) + "ms");
     }
 
     public static void definirTamanhoDaMatriz() {
@@ -73,7 +88,7 @@ public class ThreadExecute {
     }
 
     public static void preencherMatriz() {
-        Random numero = new Random(2); //inicia semente para sempre gerar a mesma matriz
+        Random numero = new Random(2); // inicia semente para sempre gerar a mesma matriz
         ArrayList<Integer> linha;
 
         if (TAM != 0) {
@@ -99,8 +114,12 @@ public class ThreadExecute {
         float iInicial, jInicial, iFinal, jFinal, indice;
         float numerosPorThreads;
 
+        start = System.currentTimeMillis();
+
         if (numeroDeThreads != 0 && matrizDeNumeros.size() != 0) {
             numerosPorThreads = (TAM * TAM) / numeroDeThreads;
+
+            ThreadRunnable.setQuantidadeDePrimos(0);
 
             for (int n = 0; n < numeroDeThreads; n++) {
                 // converte um dos termos para float pois divisão entre inteiros sempre retorna
@@ -109,17 +128,33 @@ public class ThreadExecute {
                 jInicial = (n * numerosPorThreads) % TAM;
 
                 indice = ((n + 1) * numerosPorThreads) / TAM;
-                iFinal = ((indice * 10) % 10) == 0 ? (int)indice - 1 : (int)indice;
+                iFinal = ((indice * 10) % 10) == 0 ? (int) indice - 1 : (int) indice;
 
                 indice = ((n + 1) * numerosPorThreads) % TAM;
-                jFinal = indice == 0 ? TAM - 1 : (int)indice - 1;
+                jFinal = indice == 0 ? TAM - 1 : (int) indice - 1;
                 // converte para indices inteiros
-                ThreadRunnable thread = new ThreadRunnable((int)iInicial, (int)jInicial, (int)iFinal, (int)jFinal);
+                ThreadRunnable thread = new ThreadRunnable((int) iInicial, (int) jInicial, (int) iFinal, (int) jFinal);
             }
 
         } else
             System.out.println("Numero de Threads não definida ou matriz não preenchida");
 
+        now = System.currentTimeMillis();
+    }
+
+    public static void modoSerial() {
+        start = System.currentTimeMillis();
+
+        ThreadRunnable serial = null;
+
+        ThreadRunnable.setQuantidadeDePrimos(0);
+
+        if (matrizDeNumeros.size() != 0)
+            serial = new ThreadRunnable(TAM, TAM);
+        else
+            System.out.println("matriz não preenchida");
+
+        now = System.currentTimeMillis();
     }
 
     public static void imprimirMatriz() {
--- src/com/threads/implementacao/ThreadRunnable.java
+++ src/com/threads/implementacao/ThreadRunnable.java
@@ -1,5 +1,8 @@
 package com.threads.implementacao;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -7,6 +10,9 @@ public class ThreadRunnable implements Runnable {
 
     public static int somaNumeroDePrimos = 0;
     private int iInicial, jInicial, iFinal, jFinal;
+
+    private static final Semaphore SEMAFORO = new Semaphore(1);
+    private static Lock lock = new ReentrantLock();
 
     public ThreadRunnable(int iInicial, int jInicial, int iFinal, int jFinal) {
         this.iInicial = iInicial;
@@ -18,27 +24,42 @@ public class ThreadRunnable implements Runnable {
         t.start();
     }
 
+    public ThreadRunnable(int iFinal, int jFinal) {
+        this.iInicial = 0;
+        this.jInicial = 0;
+        this.iFinal = iFinal;
+        this.jFinal = jFinal;
+
+        modoSerial(iFinal, jFinal);
+    }
+
     @Override
     public void run() {
+        lock.lock();
         int i = iInicial, j = jInicial;
-      
-        while(i != iFinal || j != jFinal+1){
+        while (i != iFinal || j != jFinal + 1) {
 
-            if(j == ThreadExecute.TAM){
+            if (j == ThreadExecute.TAM) {
                 j = 0;
-                i ++;
+                i++;
             }
-/*
-            System.out.println("[" + i + "]" + "[" + j + "] = " + ThreadExecute.matrizDeNumeros.get(i).get(j));
-*/
-            //synchronized(ThreadRunnable.class){
+
+            // System.out.println(Thread.currentThread().getName() + "[" + i + "]" + "[" + j
+            // + "] = "
+            // + ThreadExecute.matrizDeNumeros.get(i).get(j) + " - " + somaNumeroDePrimos);
+            if (isPrimo(ThreadExecute.matrizDeNumeros.get(i).get(j)))
+                somaNumeroDePrimos++;
+
+            j++;
+        }
+        lock.unlock();
+    }
+
+    public void modoSerial(int iFinal, int jFinal) {
+        for (int i = 0; i < iFinal; i++)
+            for (int j = 0; j < jFinal; j++)
                 if (isPrimo(ThreadExecute.matrizDeNumeros.get(i).get(j)))
                     somaNumeroDePrimos++;
-            //}
-
-            j ++;
-
-        } 
     }
 
     public boolean isPrimo(int numero) {
@@ -54,7 +75,11 @@ public class ThreadRunnable implements Runnable {
         return true;
     }
 
-    public static int getQuantidadeDePrimos(){
+    public static int getQuantidadeDePrimos() {
         return somaNumeroDePrimos;
+    }
+
+    public static void setQuantidadeDePrimos(int somaNumeroDePrimos) {
+        ThreadRunnable.somaNumeroDePrimos = somaNumeroDePrimos;
     }
 }
